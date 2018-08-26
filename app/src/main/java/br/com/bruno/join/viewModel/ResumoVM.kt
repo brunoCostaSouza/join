b7228c6ee0dff699ff6468c7eb0c51814e48c737
BRUNO-DELL14R\Bruno <bruno.souza.siueg@gmail>
2018-08-26 10:47:11 -0300

    icons

--- app/src/main/java/br/com/bruno/join/viewModel/ResumoVM.kt
+++ app/src/main/java/br/com/bruno/join/viewModel/ResumoVM.kt
@@ -2,12 +2,18 @@ package br.com.bruno.join.viewModel
 
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
+import br.com.bruno.join.entity.Resumo
 
 /**
  * Created by Bruno Costa on 09/08/2018.
  */
 class ResumoVM: ViewModel() {
+
+    lateinit var resumo: Resumo;
+
     var saldo = ObservableField<String>()
+    var receita = ObservableField<String>()
+    var despesas = ObservableField<String>()
 
     init {
         saldo.set("3.700,00")
